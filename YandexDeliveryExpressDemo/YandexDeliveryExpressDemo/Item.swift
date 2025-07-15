cdb08a4e210665d8f5924aaab588939d3f5f2f03
MrTeller <MrTellex@example.com>
2025-07-15 22:44:29 +0300

    Initial Commit

---
+++ YandexDeliveryExpressDemo/YandexDeliveryExpressDemo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  YandexDeliveryExpressDemo
+//
+//  Created by Paul Buktab on 7/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
